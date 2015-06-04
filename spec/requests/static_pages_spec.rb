ccb4c9ce67771753ca3dc4b09bcb0917acd234c5
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2015-06-04 10:39:26 +0900

    finished 5.3.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,29 +5,29 @@ RSpec.describe "StaticPages", type: :request do
 
   describe "GET /static_pages" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_http_status(200)
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).not_to have_title('| Home')
     end
   end
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
@@ -35,11 +35,11 @@ RSpec.describe "StaticPages", type: :request do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
@@ -47,11 +47,11 @@ RSpec.describe "StaticPages", type: :request do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
