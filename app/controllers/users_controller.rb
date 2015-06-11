8c4763a14385debfd5ee4a847f66c6fd915cd509
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2015-06-11 18:53:33 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def new
     @user = User.new
   end
@@ -38,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   private
     
     def user_params
@@ -57,4 +64,7 @@ class UsersController < ApplicationController
       redirect_to(root_path) unless current_user?(@user)
     end
 
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
