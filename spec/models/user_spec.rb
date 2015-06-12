d0255d06f9e19e2d5f4dfbce0d560dde3ae46cea
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2015-06-12 14:50:03 +0900

    finished 10.1.3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe User, type: :model do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
 
   it { should be_valid }
   it { should_not be_admin }
