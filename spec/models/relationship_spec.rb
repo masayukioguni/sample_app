84a831d210193d05013dba16f3cb06fcc3300f33
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2015-06-14 19:12:13 +0900

    finished 11.1.2

---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Relationship, type: :model do
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+
+  subject { relationship }
+
+  it { should be_valid }
+
+  describe "follower methods" do
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    its(:follower) { should eq follower }
+    its(:followed) { should eq followed }
+  end
+end
